06ba2e7358550020cb5aab584254b36e3fc3d24d
ivan_ <ivan_@Ivan>
2024-10-28 13:39:34 -0300

    intento final

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/curso-js/', // Asegúrate de usar el nombre de tu repositorio aquí
+  base: 'https://github.com/IvanBrianCruz/curso-js.git', // Asegúrate de usar el nombre de tu repositorio aquí
 });
